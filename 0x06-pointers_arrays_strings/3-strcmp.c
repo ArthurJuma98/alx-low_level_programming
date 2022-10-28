16105fd7a16d03ad782556a82a9f99b9b0dcf70f
ArthurJuma98 <arthurjuma98@gmail.com>
2022-10-28 14:00:43 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - compares the values of two strings
+ * _strcmp - compares two strings values
  * @s1: string 1
  * @s2: string 2
  * Return: the difference in s1 and s2
