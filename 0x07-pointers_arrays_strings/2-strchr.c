f642982ecc4090daf72efac725add723bf6b0c64
ArthurJuma98 <arthurjuma98@gmail.com>
2022-11-01 14:12:04 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,11 +2,11 @@
 #include <stddef.h>
 
 /**
- * _strchr - locates characters in a string
+ * _strchr - locates a character
  * @s: the string we look through
  * @c: the character we look for
  *
- * Return: a pointer to the char or null
+ * Return: a pointer to the char or NULL
  */
 
 char *_strchr(char *s, char c)
@@ -14,7 +14,7 @@ char *_strchr(char *s, char c)
 	if (s == NULL)
 		return (NULL);
 
-	for (; *s, s++)
+	for (; *s; s++)
 	{
 		if (*s == c)
 			return (s);
